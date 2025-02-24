f843b8ca875f3f1583aa56fdd99d678cc25de01d
abd-elmoughit <abdelmoughitbeilla20@gmail.com>
2025-02-24 12:46:32 +0100

    add line in program

---
+++ calcul_montant.py
@@ -0,0 +1,10 @@
+def CalculMontant(n1:float,n2:float):
+    s1=n1+n2
+    return s1
+def CalculProduit(n1:float,n2:float):
+    s2=n1*n2
+    return s2
+
+def Calculdiff(n1:float,n2:float):
+    s3=n1-n2
+    return s3
--- formWork.py
+++ formWork.py
@@ -1,15 +1,16 @@
 from os import system
-
+from calcul_montant import *
 
 choix=0
-while choix !=4:
+while choix !=5:
     system('cls')
-    print("1. calcul somme\n2. calcul montant\n3. Quitter\nTapez votre choix ?")
+    print("1. calcul somme\n2. calcul montant\n3. calcul produit\n4. calcul diff\n5. Quitter\nTapez votre choix ?")
     choix=int(input())
     if choix==1:
-      pass
+      system('cls')
     if choix==2:
-      pass
+       
+       system('cls')
     if choix==3:
-      pass
-print("fin")
+            
+      print("fin")
